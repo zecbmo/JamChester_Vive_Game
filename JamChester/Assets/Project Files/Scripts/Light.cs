29dd662c130c2b33cced9927a5778661f64100eb
Zecbmo <gmccartan05@gmail.com>
2016-05-21 21:05:18 +0100

    More assests

---
+++ JamChester/Assets/Project Files/Scripts/Light.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Light : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "FINGERPRINTS")
+        {
+            MeshRenderer mr = coll.GetComponent<MeshRenderer>();
+            mr.enabled = true;
+            FingerPrint f = coll.GetComponent<FingerPrint>();
+            f.found = true;
+        }
+    }
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject.tag == "FINGERPRINTS")
+        {
+            MeshRenderer mr = coll.GetComponent<MeshRenderer>();
+            mr.enabled = false;
+
+        }
+    }
+}
